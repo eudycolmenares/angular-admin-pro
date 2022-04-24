ca8c03b47ae368e8c601f5979ee680ded58d7b83
eudycolmenares <eudy.developer@gmail.com>
2022-04-24 13:41:54 -0500

    Child routes are added to the router

--- src/app/app-rounting.module.ts
+++ src/app/app-rounting.module.ts
@@ -7,14 +7,23 @@ import { RegisterComponent } from './auth/register/register.component';
 import { ProgressComponent } from './pages/progress/progress.component';
 import { Graphic1Component } from './pages/graphic1/graphic1.component';
 import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
+import { PagesComponent } from './pages/pages.component';
 // 
 const routes: Routes = [
-  { path: 'dashboard', component: DashboardComponent },
+  { 
+    path: '', 
+    component: PagesComponent,
+    children: [
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'progress', component: ProgressComponent },
+      { path: 'graphic1', component: Graphic1Component },
+      { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+    ]
+  },
+
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: 'progress', component: ProgressComponent },
-  { path: 'graphic1', component: Graphic1Component },
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+
   { path: '**', component: NopagefoundComponent },
 ];
 // 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
 import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-
+import { PagesComponent } from './pages/pages.component';
+// 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     SidebarComponent,
-    BreadcrumbsComponent
+    BreadcrumbsComponent,
+    PagesComponent
   ],
   imports: [
     BrowserModule,
