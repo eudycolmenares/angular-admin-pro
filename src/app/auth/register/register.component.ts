9c9b441c5f26d7350deb80e93c80adab75362681
eudycolmenares <eudy.developer@gmail.com>
2022-04-24 21:12:23 -0500

    Settings in register component

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -3,9 +3,9 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styles: [
-  ]
+  styleUrls: [ './register.component.css' ]
 })
+
 export class RegisterComponent implements OnInit {
 
   constructor() { }
